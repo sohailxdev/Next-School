ecd8fa89f100821bef71ffc4197dfd900502523d
sohailxdev <100mohdsohail@gmail.com>
2025-06-02 02:25:39 +0530

    did changes in nextconfig.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build",
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
     domains: ["res.cloudinary.com"],
